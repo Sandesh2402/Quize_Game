0cfd5d0250f60cb468273320e6fe4d367397f907
Sandesh2402 <sandeshs2402@gmail.com>
2023-11-29 18:05:58 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,163 @@
+const questions = [
+    {
+        question: "What is the purpose of the HTML `<meta>` tag?",
+        options: ["To define a list of metadata", "To link to external resources", "To display images", "To define metadata about the document"],
+        correctAnswer: "To define metadata about the document"
+    },
+
+    {
+        question: "Which JavaScript function is used to round a number to the nearest integer?",
+        options: ["ceil()", "round()", "floor()", "random()"],
+        correctAnswer: "round()"
+    },
+
+    {
+        question: "Which HTML element is used to define a hyperlink?",
+        options: ["<a>", "<link>", "<href>", "<url>"],
+        correctAnswer: "<a>"
+    },
+
+    {
+        question: "What does HTML5 introduce for embedding audio and video?",
+        options: ["<audio> and <video> elements", "<sound> and <visual> elements", "<music> and <movie> elements", "<play> and <pause> elements"],
+        correctAnswer: "<audio> and <video> elements"
+    },
+
+    {
+        question: "What does CSS `float: left;` property value do?",
+        options: ["Floats the element to the right", "Floats the element to the center", "Floats the element to the left", "Does not affect element positioning"],
+        correctAnswer: "Floats the element to the left"
+    },
+
+    {
+        question: "Which JavaScript method is used to remove the first element from an array?",
+        options: ["remove()", "pop()", "splice()", "shift()"],
+        correctAnswer: "shift()"
+    },
+
+    {
+        question: "In JavaScript, what is the purpose of the `JSON.parse()` method?",
+        options: ["To parse XML data", "To parse JSON data", "To parse HTML data", "To create a new function"],
+        correctAnswer: "To parse JSON data"
+    },
+
+    {
+        question: "Which JavaScript method is used to add an element to the end of an array?",
+        options: ["push()", "addLast()", "unshift()", "append()"],
+        correctAnswer: "push()"
+    },
+    {
+        question: "How can you center-align an element horizontally in CSS?",
+        options: ["text-align: center;", "horizontal-align: center;", "margin: 0 auto;", "align: center;"],
+        correctAnswer: "margin: 0 auto;"
+    },
+    {
+        question: "What CSS property is used to add shadow to an element's text?",
+        options: ["box-shadow", "text-shadow", "box-text", "element-shadow"],
+        correctAnswer: "text-shadow"
+    },
+
+    {
+        question: "Which HTML element is used for creating a dropdown list?",
+        options: ["<ul>", "<ol>", "<li>", "<select>"],
+        correctAnswer: "<select>"
+    },
+    {
+        question: "What is the correct way to include an external JavaScript file in an HTML document?",
+        options: ["<js src='script.js'>", "<script href='script.js'>", "<script src='script.js'>", "<javascript link='script.js'>"],
+        correctAnswer: "<script src='script.js'>"
+    },
+    {
+        question: "What is the purpose of the `localStorage` object in JavaScript?",
+        options: ["To store session data", "To store data that persists across browser sessions", "To fetch data from external sources", "To create dynamic web content"],
+        correctAnswer: "To store data that persists across browser sessions"
+    },
+
+    {
+        question: "Which CSS property is used to change the background color of an element?",
+        options: ["background-color", "color", "text-color", "bg-color"],
+        correctAnswer: "background-color"
+    },
+    {
+        question: "How do you comment out multiple lines in JavaScript?",
+        options: ["<!-- This is a comment -->", "/* This is a comment */", "// This is a comment", "/* This is a comment */ */"],
+        correctAnswer: "/* This is a comment */"
+    },
+    {
+        question: "What is the purpose of the HTML `<head>` element?",
+        options: ["To define the main content of the page", "To include JavaScript code", "To define metadata about the document", "To display images"],
+        correctAnswer: "To define metadata about the document"
+    },
+    {
+        question: "What is the CSS property used to change the size of text?",
+        options: ["font-size", "text-size", "text-style", "font-width"],
+        correctAnswer: "font-size"
+    },
+    {
+        question: "In JavaScript, how can you check the data type of a variable?",
+        options: ["typeof", "datatype", "typeOf", "typeofvariable"],
+        correctAnswer: "typeof"
+    },
+    {
+        question: "Which operator is used for strict equality in JavaScript?",
+        options: ["===", "==", "=", "!="],
+        correctAnswer: "==="
+    },
+    {
+        question: "What does the CSS `margin` property control?",
+        options: ["The spacing outside an element", "The spacing inside an element", "The element's background color", "The element's text color"],
+        correctAnswer: "The spacing outside an element"
+    },
+    {
+        question: "Which HTML tag is used for creating an ordered list?",
+        options: ["<ul>", "<ol>", "<li>", "<dl>"],
+        correctAnswer: "<ol>"
+    },
+    {
+        question: "What is the purpose of the HTML `<iframe>` element?",
+        options: ["To create an inline image", "To embed external web content within a page", "To define a clickable hyperlink", "To style text"],
+        correctAnswer: "To embed external web content within a page"
+    },
+    {
+        question: "What does CSS specificity refer to?",
+        options: ["The importance of a CSS property", "The order of CSS rules in a stylesheet", "The weight of a CSS rule in relation to other rules", "The length of a CSS property value"],
+        correctAnswer: "The weight of a CSS rule in relation to other rules"
+    },
+    {
+        question: "In JavaScript, what is the purpose of the `setTimeout()` function?",
+        options: ["To set a timer for asynchronous code execution", "To stop the execution of a function", "To change the page's URL", "To create an alert box"],
+        correctAnswer: "To set a timer for asynchronous code execution"
+    },
+    {
+        question: "What is the purpose of the HTML `<canvas>` element?",
+        options: ["To draw graphics and animations", "To create tables", "To play audio and video", "To display text"],
+        correctAnswer: "To draw graphics and animations"
+    },
+    {
+        question: "Which CSS property is used to control the spacing between lines of text?",
+        options: ["line-height", "font-spacing", "text-spacing", "line-spacing"],
+        correctAnswer: "line-height"
+    },
+    {
+        question: "In JavaScript, what is the purpose of the `parseInt()` function?",
+        options: ["To convert a string to a floating-point number", "To convert a string to an integer", "To convert a number to a string", "To round a number to the nearest integer"],
+        correctAnswer: "To convert a string to an integer"
+    },
+    {
+        question: "What is the default display property for a `<div>` element in CSS?",
+        options: ["block", "inline", "inline-block", "flex"],
+        correctAnswer: "block"
+    },
+    {
+        question: "Which JavaScript method is used to remove the last element from an array?",
+        options: ["remove()", "pop()", "splice()", "shift()"],
+        correctAnswer: "pop()"
+    },
+    {
+        question: "Which HTML tag is used for creating a table row?",
+        options: ["<table>", "<tr>", "<td>", "<th>"],
+        correctAnswer: "<tr>"
+    },
+];
+
+
